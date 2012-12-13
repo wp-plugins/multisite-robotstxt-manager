b7d9b7b16e4a9dea865471644e4a36199077e5eb
tribalNerd <tribalNerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-13 20:39:12 +0000

    Adding Version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/multisite-robotstxt-manager/tags/0.3@638832 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -6,39 +6,59 @@
  * @copyright Copyright (c) 2012, Chris Winters
  * @link http://technerdia.com/projects/robotstxt/plugin.html
  * @license http://www.gnu.org/licenses/gpl.html
- * @version 0.2.0
+ * @version 0.3
  */
 
 /**
- * ==================================== Uninstall Function
+ * ==================================== Plugin Uninstall Function
  */
 
 if ( !defined( 'ABSPATH' ) ) { exit; } /* Wordpress check */
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; } /* Kill direct access */
 
+/**
+ * Core Function
+ */
 function uninstall_ms_robotstxt() {
-	if ( !is_user_logged_in() ) { return; }
+	/* multisite function */
+	if ( !function_exists('is_super_admin') ) {
+		if ( !is_super_admin() ) { return; }
+	}
+
+	/* proper user levels only */
+	if ( !is_user_logged_in() && !current_user_can( 'manage_options' ) ) { return; }
+
+	/* query wp db */
 	global $wpdb;
+	$get_blog_ids = $wpdb->get_results( 'SELECT blog_id FROM '. $wpdb->blogs .'  ORDER BY blog_id' );
 
-	$robotstxt_remove_list = $wpdb->get_results( $wpdb->prepare( 'SELECT blog_id FROM '. $wpdb->blogs .'  ORDER BY blog_id' ) );
-
-		foreach ( $robotstxt_remove_list as $siteids ) {
-			switch_to_blog( $siteids->blog_id );
+		/* remove options for each blog id */
+		foreach ( $get_blog_ids AS $returned_id ) {
+			$this_blog_id = $returned_id->blog_id;
+			switch_to_blog($this_blog_id);
 				remove_filter( 'robots_txt', array( 'robotstxt_ms', 'do_robots_display' ) );
-				delete_option( "ms_robotstxt_sitemap" );
-				delete_option( "ms_robotstxt_default" );
-				delete_option( "ms_robotstxt" );
+				delete_option( 'ms_robotstxt_default' );
+				delete_option( 'ms_robotstxt_sitemap' );
+				delete_option( 'ms_robotstxt_auto' );
+				delete_option( 'ms_robotstxt_pro' );
+				delete_option( 'ms_robotstxt' );
 		}
 
+		/* make sure options are removed from root network site */
 		switch_to_blog(1);
 			remove_filter( 'robots_txt', array( 'robotstxt_ms', 'do_robots_display' ) );
-			delete_option( "ms_robotstxt_sitemap" );
-			delete_option( "ms_robotstxt_default" );
-			delete_option( "ms_robotstxt" );
+			delete_option( 'ms_robotstxt_default' );
+			delete_option( 'ms_robotstxt_sitemap' );
+			delete_option( 'ms_robotstxt_auto' );
+			delete_option( 'ms_robotstxt_pro' );
+			delete_option( 'ms_robotstxt' );
+
 		restore_current_blog();
 
+	/* return to wordpress */
 	return;
 }
 
-/* Call Uninstall */
+/* Run uninstall when called */
 uninstall_ms_robotstxt();
 ?>
