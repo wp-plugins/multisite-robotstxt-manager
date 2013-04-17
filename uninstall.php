7860328eb2483510f5d1f3500683834ec197cd43
tribalNerd <tribalNerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-17 15:20:22 +0000

    Version 0.4.0
    git-svn-id: https://plugins.svn.wordpress.org/multisite-robotstxt-manager/tags/0.4.0@699147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,42 +3,53 @@
  * Multisite Robots.txt Manager
  * @package Multisite Robots.txt Manager
  * @author tribalNerd (tribalnerd@technerdia.com)
- * @copyright Copyright (c) 2012, Chris Winters
- * @link http://technerdia.com/projects/robotstxt/plugin.html
+ * @copyright Copyright (c) 2012-2013, techNerdia LLC.
+ * @link http://msrtm.technerdia.com/
  * @license http://www.gnu.org/licenses/gpl.html
- * @version 0.2.0
+ * @version 0.4.0
  */
 
 /**
- * ==================================== Uninstall Function
+ * ===================================================== Plugin Uninstall Function
  */
 
-if ( !defined( 'ABSPATH' ) ) { exit; } /* Wordpress check */
+if( !defined( 'ABSPATH' ) ) { exit; } 					/* Wordpress check */
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; } 	/* Kill direct access */
 
-function uninstall_ms_robotstxt() {
-	if ( !is_user_logged_in() ) { return; }
-	global $wpdb;
+function msrtm_uninstall() {
+	if( !is_user_logged_in() && !current_user_can('manage_options') ) { wp_die( __( 'Authorized Access Required', 'ms_robotstxt_manager' ) ); }
 
-	$robotstxt_remove_list = $wpdb->get_results( $wpdb->prepare( 'SELECT blog_id FROM '. $wpdb->blogs .'  ORDER BY blog_id' ) );
+	/** if not multisite, return to avoid function errors */
+	if( !function_exists( 'switch_to_blog' ) ) { return; }
 
-		foreach ( $robotstxt_remove_list as $siteids ) {
-			switch_to_blog( $siteids->blog_id );
-				remove_filter( 'robots_txt', array( 'robotstxt_ms', 'do_robots_display' ) );
-				delete_option( "ms_robotstxt_sitemap" );
-				delete_option( "ms_robotstxt_default" );
-				delete_option( "ms_robotstxt" );
+		/** query db */
+		global $wpdb;
+		$blog_ids = $wpdb->get_results( 'SELECT blog_id FROM '. $wpdb->blogs .' ORDER BY blog_id' );
+
+		/** remove from each blog */
+		foreach ( $blog_ids as $value ) {
+			$id = $value->blog_id;
+			switch_to_blog($id);
+				remove_filter( 'robots_txt', array( 'msrtm_robots_txt', 'msrtm_show_robots_txt' ) );
+				delete_option( 'ms_robotstxt_default' );
+				delete_option( 'ms_robotstxt_sitemap' );
+				delete_option( 'msrtm_plugin_check' );
+				delete_option( 'msrtm_rules_check' );
+				delete_option( 'ms_robotstxt' );
 		}
 
+		/** make sure removed from root network site */
 		switch_to_blog(1);
-			remove_filter( 'robots_txt', array( 'robotstxt_ms', 'do_robots_display' ) );
-			delete_option( "ms_robotstxt_sitemap" );
-			delete_option( "ms_robotstxt_default" );
-			delete_option( "ms_robotstxt" );
+			remove_filter( 'robots_txt', array( 'msrtm_robots_txt', 'msrtm_show_robots_txt' ) );
+			delete_option( 'ms_robotstxt_default' );
+			delete_option( 'ms_robotstxt_sitemap' );
+			delete_option( 'msrtm_plugin_check' );
+			delete_option( 'msrtm_rules_check' );
+			delete_option( 'ms_robotstxt' );
 		restore_current_blog();
 
 	return;
 }
-
-/* Call Uninstall */
-uninstall_ms_robotstxt();
+/** run uninstall function */
+msrtm_uninstall();
 ?>
